1a0e8907e6547b1d1bb387ece47bd2e53af9fcae
kennedy muthuri <muthurikennedy082@gmail.com>
2023-06-25 09:19:15 +0300

    working on fetching data on both server and client side

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,23 +1,44 @@
-import React from 'react';
-
+"use client"
+import React from "react";
+import useSWR from 'swr';
 
 async function getData() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts",{cache:"no-store"});
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "no-store",
+  });
 
-  // Recommendation: handle errors
   if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
   }
 
   return res.json();
 }
 export default function Dashboard() {
-  return (
-    <>
-    
-    </>
-  );
+ // const [data, setData] = React.useState([]);
+  // const [error, setError] = React.useState(false);
+  // const [isLoading, setIsLoading] = React.useState(false);
+  // React.useEffect(() => {
+  //   const getData = async () => {
+  //     setIsLoading(true);
+  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  //       cache: "no-store",
+  //     });
+  //     if (!res.ok) {
+  //       setError(true);
+  //     }
+  //   const data = await res.json();
+  //     setData(data);
+  //     setIsLoading(false);
+  //   };
+  //   getData();
+  // }, []);
+  const fetcher = (...args) => fetch(...args).then(res => res.json());
+  const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
+ 
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+
+  console.log(data);
+  
+  return <></>;
 }
--- src/app/portfolio/[categoryName]/page.jsx
+++ src/app/portfolio/[categoryName]/page.jsx
@@ -2,61 +2,42 @@ import React from "react";
 import styles from "./page.module.css";
 import Button from "@/components/Button/Button";
 import Image from "next/image";
+import { items } from "./data";
+import { notFound } from "next/navigation";
+
+const getData = (category) => {
+  const data = items[category];
+  if (data) {
+    return data;
+  }
+  return notFound();
+};
 
 export default function Category({ params }) {
   console.log(params.categoryName);
+  const data = getData(params.categoryName);
 
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.categoryName}</h1>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Description</p>
-          <Button url="#" text="See More" />
+      {data.map((item) => (
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button url="#" text="See More" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image
+              src={item.image}
+              alt=""
+              className={styles.img}
+              width={500}
+              height={500}
+            />
+          </div>
         </div>
-        <div className={styles.imgContainer}>
-          <Image
-            src="/1.jpg"
-            alt=""
-            className={styles.img}
-            width={500}
-            height={500}
-          />
-        </div>
-      </div>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Description</p>
-          <Button url="#" text="See More" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-            src="/1.jpg"
-            alt=""
-            className={styles.img}
-            width={500}
-            height={500}
-          />
-        </div>
-      </div>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Description</p>
-          <Button url="#" text="See More" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-            src="/1.jpg"
-            alt=""
-            className={styles.img}
-            width={500}
-            height={500}
-          />
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
